351c3e59392aaf655def1ae27b676a4a5dead6ad
Charles Wong <charleswonghm@gmail.com>
2023-11-27 19:21:02 -0500

    Refresh landing page

--- components/Screen.tsx
+++ components/Screen.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-function Screen({ children, style }) {
+function Screen({ children, style={} }) {
     return(
         <View style={[styles.container, style]}>
             {children}
